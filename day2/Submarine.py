953a1f3b26ce3a6cc8b762e73a2a0123b57c9dfd
rocketstack-matt <matthew.bain@rocketstack.co>
2021-12-16 08:49:48 +0000

    Day 2, part 2

--- day2/Submarine.py
+++ day2/Submarine.py
@@ -3,15 +3,20 @@ class Submarine:
     def __init__(self):
         self.horizontal_position = 0
         self.vertical_position = 0
+        self.aim = 0
+        self.depth = 0
 
     def forward(self, value: int):
         self.horizontal_position += value
+        self.depth += (self.aim * value)
 
     def up(self, value: int):
         self.vertical_position -= value
+        self.aim -= value
 
     def down(self, value: int):
         self.vertical_position += value
+        self.aim += value
 
     def parse_instruction(self, instruction):
         instructions = instruction.strip().split()
